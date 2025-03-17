f9f87b6f5c67137ecf686bcdf46fd45f836d7cc0
KingSammy2025 <o.ibikunle-philips@share.epsb.ca>
2025-03-17 14:57:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// DRAW ROBOT FACE
+ // DRAW ROBOT FACE
 
 // Set up Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
@@ -8,44 +8,22 @@ cnv.height = 600;
 
 // Rectangle Behind Head for Ears
 ctx.fillStyle = "rgb(100, 100, 100)";
-ctx.fillRect(80, 260, 460, 80);
+rectangle(80, 260, 460, 80, "fill");
 
 // Filled Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(180, 80);
-ctx.lineTo(140, 100);
-ctx.lineTo(220, 100);
-ctx.fill();
+Triangle(180, 80, 140, 100, 220, 100, "fill");
 
 // Filled Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(340, 80);
-ctx.lineTo(300, 100);
-ctx.lineTo(380, 100);
-ctx.fill();
+Triangle(340, 80, 300, 100, 380, 100, "fill");
 
 // Outlined Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(260, 80);
-ctx.lineTo(220, 100);
-ctx.lineTo(300, 100);
-ctx.closePath();
-ctx.stroke();
+Triangle(260, 80, 220, 100, 300, 100, "stroke");
 
 // Outlined Triangle for Hair
-ctx.beginPath();
-ctx.moveTo(420, 80);
-ctx.lineTo(380, 100);
-ctx.lineTo(460, 100);
-ctx.closePath();
-ctx.stroke();
+Triangle(420, 80, 380, 100, 460, 100, "stroke");
 
 // Filled Triangle for Neck
-ctx.beginPath();
-ctx.moveTo(300, 200);
-ctx.lineTo(220, 600);
-ctx.lineTo(380, 600);
-ctx.fill();
+Triangle(300, 200, 220, 600, 380, 600, "fill");
 
 // Rectangle Head
 ctx.fillStyle = "rgb(180, 180, 180)";
@@ -57,37 +35,22 @@ circle(200, 250, 50, "fill");
 
 // Filled Circle for Right Eye Socket
 circle(400, 250, 50, "fill");
-ctx.beginPath();
-ctx.arc(400, 250, 50, 0, 2 * Math.PI);
-ctx.fill();
 
 // Rectangle for Mouth
 rectangle(200, 350, 200, 60, "fill");
 
 // Horizontal Line for Teeth
 ctx.strokeStyle = "rgb(180, 180, 180)";
-ctx.beginPath();
-ctx.moveTo(200, 380);
-ctx.lineTo(400, 380);
-ctx.stroke();
+line(200, 380, 400, 380, "stroke");
 
 // First Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(250, 350);
-ctx.lineTo(250, 410);
-ctx.stroke();
+line(250, 350, 250, 410, "stroke");
 
 // Second Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(300, 350);
-ctx.lineTo(300, 410);
-ctx.stroke();
+line(300, 350, 300, 410, "stroke");
 
 // Third Vertical Line for Teeth
-ctx.beginPath();
-ctx.moveTo(350, 350);
-ctx.lineTo(350, 410);
-ctx.stroke();
+line(350, 350, 350, 410, "stroke");
 
 // Filled Circle for Left Eye
 ctx.fillStyle = "rgb(100, 100, 100)";
@@ -105,25 +68,29 @@ circle(400, 250, 50, "stroke");
 
 // Line for Left Eyebrow
 ctx.lineWidth = 2;
-// line(150, 180, 250, 180, "stroke");
-ctx.beginPath();
-ctx.moveTo(150, 180);
-ctx.lineTo(250, 180);
-ctx.stroke();
+line(150, 180, 250, 180, "stroke");
 
 // Line for Right Eyebrow
-ctx.beginPath();
-ctx.moveTo(350, 160);
-ctx.lineTo(450, 180);
-ctx.stroke();
+line(350, 160, 450, 180, "stroke");
 
 // Outlined Triangle for Nose
+Triangle(300, 280, 320, 320, 280, 320, "stroke", "close");
+
+// Eye patch
+ctx.fillStyle = "blue";
 ctx.beginPath();
-ctx.moveTo(300, 280);
-ctx.lineTo(320, 320);
-ctx.lineTo(280, 320);
-ctx.closePath();
-ctx.stroke();
+ctx.arc(200, 200, 100, 0, 1 * Math.PI);
+ctx.fill();
+
+// Headband
+ctx.fillStyle = "red";
+rectangle(100, 100, 400, 40, "fill");
+
+// Goatee
+ctx.fillStyle = "black";
+ctx.beginPath();
+ctx.arc(300, 440, 50, 0, 1 * Math.PI);
+ctx.fill();
 
 // Helper functions
 function circle(x, y, r, type) {
@@ -146,10 +113,24 @@ function rectangle(x, y, w, h, type) {
   }
 }
 
-function line(x1, y1, x2, y2) {
+function line(x1, y1, x2, y2, type) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
+  if (type == "fill") {
+    ctx.fill();
+  } else if (type == "stroke") {
+    ctx.stroke();
+  }
+}
+
+function Triangle(x1, y1, x2, y2, x3, y3, type) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.lineTo(x3, y3);
+  ctx.closePath();
+
   if (type == "fill") {
     ctx.fill();
   } else if (type == "stroke") {
